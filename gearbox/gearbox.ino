f9e328f221ca4b850fa17101954e4db57c00d226
Jisu Sim <rlj1202@gmail.com>
2018-07-14 17:20:16 +0900

    Semi-final code
    PID control is needed.

--- gearbox/gearbox.ino
+++ gearbox/gearbox.ino
@@ -1,3 +1,82 @@
+/*
+ * A-FA GearBox using Arduino Uno
+ * 
+ * 
+ * 
+ * JisuSim, ByeongHeeJang
+ * Ajou University, Korea, 2018.
+ */
+
+static const float PWM_HZ = 12.5;// to control pnuematic piston pressure.
+
+static const int PIN_OUT_RELAY_BACK_IN = 2;
+static const int PIN_OUT_RELAY_BACK_OUT = 3;
+static const int PIN_OUT_RELAY_FRONT_IN = 4;
+static const int PIN_OUT_RELAY_FRONT_OUT = 5;
+
+static const int PIN_IN_BUTTON_1 = 11;
+static const int PIN_IN_BUTTON_2 = 12;
+
+static const int PIN_IN_TARGET = A0;
+static const int PIN_IN_POSITION = A1;
+
+static const int WAIT_VALUE_ARRAY_SIZE = 25;
+
+/*
+class DelayTaskNode {
+  public:
+  DelayTaskNode(int milli, bool(*func)()) {
+    this->milli = milli;
+    this->func = func;
+  }
+  DelayTaskNode *next;
+  DelayTaskNode *prev;
+  long startTime;
+  private:
+  int milli;
+  bool(*func)();
+};
+
+DelayTaskNode *root = NULL;
+
+void addDelayTask(int milli, bool (*func)()) {
+  DelayTaskNode *newTask = new DelayTaskNode(milli, func);
+  newTask->startTime = millis();
+  
+  if (root == NULL) {
+    root = newTask;
+  } else {
+    DelayTaskNode *lastNode = root;
+    while (true) {
+      if (lastNode->next != NULL) {
+        lastNode = lastNode->next;
+      } else {
+        break;
+      }
+    }
+    lastNode->next = newTask;
+    newTask->prev = lastNode;
+  }
+}
+
+void precceedDelayTasks() {
+  int curTime = millis();
+  DelayTaskNode *curNode = root;
+  while (curNode != NULL) {
+    long elapsed = curTime - curNode->startTime;
+    if (elapsed >= curNode->milli) {
+      curNode->func();
+      if (curNode->prev != NULL && curNode->next != NULL) {
+        curNode->prev->next = curNode->next;
+        curNode->next->prev = curNode->prev;
+      }
+    }
+    curNode = curNode->next;
+  }
+}*/
+
+long waitValues[WAIT_VALUE_ARRAY_SIZE];
+
 int lastDelta = 0;
 
 int relay1Pr = 16;
@@ -13,37 +92,167 @@ int button2State = LOW;
 
 int serialTime = 0;
 
-void setup() {
-  pinMode(12, INPUT);
-  pinMode(11, INPUT);
-
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-
-  pinMode(8, OUTPUT);
-  pinMode(9, INPUT);
-
-  digitalWrite(5, HIGH);
-
-  Serial.begin(9600);
-  while (!Serial) {
+// DEPRECATED
+void initWaitFunc() {
+  for (int i = 0; i < WAIT_VALUE_ARRAY_SIZE; i++) {
+    waitValues[i] = -1;
   }
 }
 
-void loop() {
-  digitalWrite(8, HIGH);
-  delay(10);
-  digitalWrite(8, LOW);
+// DEPRECATED
+// wait function invokes given function at every given milli seconds.
+template<typename Func> bool wait(int index, long milli, Func f) {
+  long curTime = millis();
+  long lastTime = waitValues[index];
+  if (lastTime == -1) {
+    lastTime = curTime;
+    waitValues[index] = curTime;
+  }
+  long elapsed = curTime - lastTime;
 
-  float distance = analogRead(A1) / 1024.0 * 4.5;
-  float target = analogRead(A0) / 1024.0 * 4.5;
-  float delta = target - distance;
+  if (elapsed >= milli) {
+    bool result = f();
+    if (result == true) {
+      waitValues[index] = millis();
+    }
+    return result;
+  }
+
+  return false;
+}
+
+void setup() {
+  // Initialize I/O pins.
+  pinMode(PIN_OUT_RELAY_BACK_IN, OUTPUT);
+  pinMode(PIN_OUT_RELAY_BACK_OUT, OUTPUT);
+  pinMode(PIN_OUT_RELAY_FRONT_IN, OUTPUT);
+  pinMode(PIN_OUT_RELAY_FRONT_OUT, OUTPUT);
+  digitalWrite(PIN_OUT_RELAY_BACK_IN, LOW);
+  digitalWrite(PIN_OUT_RELAY_BACK_OUT, LOW);
+  digitalWrite(PIN_OUT_RELAY_FRONT_IN, LOW);
+  digitalWrite(PIN_OUT_RELAY_FRONT_OUT, LOW);
+
+  pinMode(PIN_IN_BUTTON_1, INPUT);
+  pinMode(PIN_IN_BUTTON_2, INPUT);
+
+  // Initialize wait function.
+  initWaitFunc();
+
+  // Initialize Serial communication.
+  Serial.begin(9600);
+  while (!Serial) {// Wait until serial initialization is done.
+  }
+}
+
+void __loop() {
   
-  //float delayTime = max(abs(delta) * 1.0 - 2 / abs(lastDelta - delta), 60.0);// PID
-  float delayTime = 20.0;
+}
 
+void loop() {
+  for (int i = 0; i < 5; i++) {
+    /*digitalWrite(PIN_OUT_RELAY_FRONT_OUT, HIGH);
+    delay(20);
+    digitalWrite(PIN_OUT_RELAY_FRONT_OUT, LOW);
+    delay(500);*/
+    digitalWrite(PIN_OUT_RELAY_FRONT_IN, HIGH);
+    delay(50);
+    digitalWrite(PIN_OUT_RELAY_FRONT_IN, LOW);
+    delay(500);
+    digitalWrite(PIN_OUT_RELAY_BACK_IN, HIGH);
+    delay(10);
+    digitalWrite(PIN_OUT_RELAY_BACK_IN, LOW);
+    delay(500);
+  }
+  delay(2000);
+  for (int i = 0; i < 30; i++) {
+    digitalWrite(PIN_OUT_RELAY_FRONT_IN, HIGH);
+  digitalWrite(PIN_OUT_RELAY_BACK_OUT, HIGH);
+  delay(60);
+  digitalWrite(PIN_OUT_RELAY_FRONT_IN, LOW);
+  digitalWrite(PIN_OUT_RELAY_BACK_OUT, LOW);
+  delay(20);
+  }
+  delay(2000);
+  Serial.println("Test");
+}
+
+void _loop() {
+  float distance = analogRead(A0) / 1024.0 * 4.5;
+  //float target = analogRead(A1) / 1024.0 * 4.5;
+  float target = (sin(millis() / 1000.0 * 4) + 1.0) / 2.0 * 4.5;
+  float delta = distance - target;
+  
   float lastDelta = delta;
+  long delayTime = abs(delta / 4.5 * 70.0 + 10);
 
+  Serial.print(target);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(",");
+  Serial.print(delta);
+  Serial.println();
+  
+  /*
+  wait(0, 500, [&delayTime]() -> bool {
+      Serial.println("LOW");
+      digitalWrite(PIN_OUT_RELAY_BACK, LOW);
+      return wait(1, 1000, []() -> bool {
+        Serial.println("HIGH");
+        digitalWrite(PIN_OUT_RELAY_BACK, HIGH);
+        return true;
+      });
+    });*/
+    digitalWrite(PIN_OUT_RELAY_FRONT_IN, LOW);
+  if (abs(delta) > 0.5) {
+    //long delayTime = exp((delta - 4.5) / 3.0) * 80.0;
+    /*
+    
+    
+      digitalWrite(PIN_OUT_RELAY_BACK_IN, LOW);
+      delay(delayTime);
+      digitalWrite(PIN_OUT_RELAY_BACK_IN, HIGH);
+      delay(80 - delayTime);*/
+    
+    if (delta < 0) {// down
+      digitalWrite(PIN_OUT_RELAY_BACK_IN, LOW);
+      delay(10);
+      digitalWrite(PIN_OUT_RELAY_BACK_IN, HIGH);
+      delay(70);
+      /*
+      digitalWrite(PIN_OUT_RELAY_FRONT_OUT, LOW);
+      delay(3);
+      digitalWrite(PIN_OUT_RELAY_FRONT_OUT, HIGH);
+      delay(1000);
+      digitalWrite(PIN_OUT_RELAY_BACK_IN, LOW);
+      delay(10);
+      digitalWrite(PIN_OUT_RELAY_BACK_IN, HIGH);
+      delay(1000);*/
+      /*
+      digitalWrite(PIN_OUT_RELAY_FRONT_OUT, LOW);
+      digitalWrite(PIN_OUT_RELAY_BACK_IN, LOW);
+      delay(20);
+      digitalWrite(PIN_OUT_RELAY_FRONT_OUT, HIGH);
+      digitalWrite(PIN_OUT_RELAY_BACK_IN, HIGH);
+      delay(60);*/
+    } else {// UP
+      digitalWrite(PIN_OUT_RELAY_FRONT_IN, LOW);
+      digitalWrite(PIN_OUT_RELAY_BACK_OUT, LOW);
+      delay(20);
+      //digitalWrite(PIN_OUT_RELAY_FRONT_IN, HIGH);
+      digitalWrite(PIN_OUT_RELAY_BACK_OUT, HIGH);
+      delay(60);
+    }
+  }
+  
+/*
+  Serial.print(target);
+  Serial.print(", ");
+  Serial.print(distance);
+  Serial.print(", ");
+  Serial.print(delta);
+  Serial.println();*/
+
+/*
   if (abs(delta) > 0.5) {
     if (delta > 0) {
     digitalWrite(3, LOW);
@@ -55,7 +264,7 @@ void loop() {
   } else {
     digitalWrite(3, HIGH);
     digitalWrite(5, HIGH);
-  }
+  }*/
 
 /*
   if (abs(delta)>0.3) {
@@ -102,16 +311,5 @@ void loop() {
     digitalWrite(5, LOW);
     button2Time = 0;
   }*/
-  int button1 = digitalRead(12);
-  int button2 = digitalRead(11);
-  
-  Serial.print(target);
-  Serial.print(", ");
-  Serial.print(distance);
-  Serial.print(", ");
-  Serial.print(delta);
-  //Serial.print(", ");
-  //Serial.print(delayTime);
-  Serial.println();
 }
 
